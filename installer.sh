0394de31da36e9bc4d5ec4abd2a7e3412d02ce55
jw2476 <jackwh.whitehorn@gmail.com>
2018-11-18 13:15:27 +0000

    FFMpeg now installs with mp3 codecs

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ function InstallFFmpeg(){
 	
 	sudo apt-get update
 	sudo apt-get upgrade -y
-	sudo apt-get install autoconf automake build-essential git libass-dev libfreetype6-dev libomxil-bellagio-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config tar texinfo wget zlib1g-dev -y
+	sudo apt-get install autoconf automake build-essential libass-dev libfreetype6-dev libmp3lame-dev libomxil-bellagio-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev -y
 	
 	echo "Clone git repo"
 	
@@ -49,7 +49,7 @@ function InstallFFmpeg(){
 	echo "Compile FFMPEG"
 	
 	cd ..
-	./configure --prefix=/home/pi/ffmpeg/dependencies/output --enable-gpl --enable-libx264 --enable-nonfree --enable-mmal --enable-libfdk_aac --enable-omx --enable-omx-rpi --extra-cflags="-I/home/pi/ffmpeg/dependencies/output/include" --extra-ldflags="-L/home/pi/ffmpeg/dependencies/output/lib" --extra-libs="-lx264 -lpthread -lm -ldl"
+	./configure --prefix=/home/pi/ffmpeg/dependencies/output --enable-gpl --enable-libx264 --enable-nonfree --enable-mmal --enable-libfdk_aac --enable-omx --enable-omx-rpi --enable-libmp3lame --extra-cflags="-I/home/pi/ffmpeg/dependencies/output/include" --extra-ldflags="-L/home/pi/ffmpeg/dependencies/output/lib" --extra-libs="-lx264 -lpthread -lm -ldl"
 	make -j4
 	make install
 	
