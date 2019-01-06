8ed5e09273b2cf60276f16795359350f52c836c8
Gabriel Kyne <gabriel@kyne.co.uk>
2019-01-06 10:46:36 +0000

    Weight tare resolved. Chnages to weight.py and hx711.py

---
+++ v0.5/hedge.py
@@ -0,0 +1,28 @@
+import post
+import video
+import subprocess
+import thermo
+import weight
+
+x = input("w = weight \nt = temp \np = post \nv = video \n")
+
+if x == "t":
+    temperature = thermo.sensor()
+    temperature.write('temp.csv', True)
+    temperature.avrg('temp.csv', 'avrtemp.csv', True)
+
+elif x == "v":
+    test = subprocess.Popen(['python3', 'video.py'])
+    test.wait()
+
+elif x == "w":
+    weight_sensor = weight.sensor()
+    weight_sensor.tare_weight(0.6)
+    weight_sensor.read(True)
+    weight_sensor.write('weight.csv', True)
+    weight_sensor.avrg('weight.csv', 'avrweight.csv', 0.95, True)
+    # weight_sensor.tare_weight(100)  # 100 = min tolerance
+
+elif x == "p":
+    web = post.http()
+    web.post('http://10.172.100.68', 'test', 'test', True)
