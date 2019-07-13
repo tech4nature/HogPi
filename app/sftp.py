556a05652d5f7c3f01145236c5969cc2ea2b6c3c
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2019-07-13 12:24:03 +0100

    Fixed test

--- app/sftp.py
+++ app/sftp.py
@@ -5,8 +5,7 @@ def pull_videos(ip, username, password):
     remoteCam = pysftp.Connection(ip, username=username, password=password)
     remoteCam.cd('/home/pi/Videos')
     files = remoteCam.list_dir()
-
     for file in files:
-            if '.mp4' in file:
-                remoteCam.get(file, '/home/pi/Videos')
-                remoteCam.remove(file)
+        if 'mp4' in file:
+            remoteCam.get(file, '/home/pi/Videos')
+            remoteCam.remove(file)
--- app/tests/test_sftp.py
+++ app/tests/test_sftp.py
@@ -2,21 +2,25 @@ import sftp
 
 from unittest.mock import ANY
 from unittest.mock import patch
+import unittest.mock
 
 
 @patch('sftp.pysftp')
-def test_with_mp4(mock_pysftp):
-    sftp.pull_videos('8.8.8.8', 'john', 'password')
-    mock_pysftp.Connection.assert_called()
-    mock_pysftp.Connection.cd.assert_called()
-    mock_pysftp.Connection.list_dir.return_value = ['test.mp4', 'test1.mp4']
-    assertEqual(mock_pysftp.Connection.get.call_count, 2)
+class sftp_tests(unittest.TestCase):
+    def test_with_mp4(self, mock_pysftp):
+        mock_pysftp.Connection().list_dir.return_value = ['test.mp4', 'test1.mp4']
+        sftp.pull_videos('8.8.8.8', 'john', 'password')
+        mock_pysftp.Connection.assert_called()
+        mock_pysftp.Connection().cd.assert_called()
+        mock_pysftp.Connection().get.assert_called()
+
+    def test_without_mp4(self, mock_pysftp):
+        mock_pysftp.Connection().list_dir.return_value = ['test.mp3', 'test1.mov']
+        sftp.pull_videos('8.8.8.8', 'john', 'password')
+        mock_pysftp.Connection.assert_called()
+        mock_pysftp.Connection().cd.assert_called()
+        self.assertEqual(mock_pysftp.Connection().get.call_count, 0)
 
 
-@patch('sftp.pysftp')
-def test_without_mp4(mock_pysftp):
-    sftp.pull_videos('8.8.8.8', 'john', 'password')
-    mock_pysftp.Connection.assert_called()
-    mock_pysftp.Connection.cd.assert_called()
-    mock_pysftp.Connection.list_dir.return_value = ['test.mp3', 'test1.mov']
-    assertEqual(mock_pysftp.Connection.get.call_count, 0)
+if __name__ == '__main__':
+    unittest.main()
