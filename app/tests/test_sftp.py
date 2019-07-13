92896d20d894e4160051e89c1f7fdaa31f0c5d3f
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2019-07-13 13:08:13 +0100

    Made codecov 100%

--- app/tests/test_sftp.py
+++ app/tests/test_sftp.py
@@ -1,11 +1,10 @@
 import sftp
-
 from unittest.mock import ANY
 from unittest.mock import patch
 
 
 @patch('sftp.pysftp')
-def test_with_mp4(self, mock_pysftp):
+def test_with_mp4(mock_pysftp):
     mock_pysftp.Connection().list_dir.return_value = ['test.mp4', 'test1.mp4']
     sftp.pull_videos('8.8.8.8', 'john', 'password')
     mock_pysftp.Connection.assert_called()
@@ -14,7 +13,7 @@ def test_with_mp4(self, mock_pysftp):
 
 
 @patch('sftp.pysftp')
-def test_without_mp4(self, mock_pysftp):
+def test_without_mp4(mock_pysftp):
     mock_pysftp.Connection().list_dir.return_value = ['test.mp3', 'test1.mov']
     sftp.pull_videos('8.8.8.8', 'john', 'password')
     mock_pysftp.Connection.assert_called()
