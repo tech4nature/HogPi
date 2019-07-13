fd733c660ed17d86bacef812a49731257dee3514
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2019-07-13 11:53:17 +0100

    Added test for SFTP

---
+++ app/tests/test_sftp.py
@@ -0,0 +1,22 @@
+import sftp
+
+from unittest.mock import ANY
+from unittest.mock import patch
+
+
+@patch('sftp.pysftp')
+def test_with_mp4(mock_pysftp):
+    sftp.pull_video('8.8.8.8', 'john', 'password')
+    mock_pysftp.Connection.assert_called()
+    mock_pysftp.Connection.cd.assert_called()
+    mock_pysftp.Connection.list_dir.return_value = ['test.mp4', 'test1.mp4']
+    assertEqual(mock_pysftp.Connection.get.call_count, 2)
+
+
+@patch('sftp.pysftp')
+def test_without_mp4(mock_pysftp):
+    sftp.pull_video('8.8.8.8', 'john', 'password')
+    mock_pysftp.Connection.assert_called()
+    mock_pysftp.Connection.cd.assert_called()
+    mock_pysftp.Connection.list_dir.return_value = ['test.mp3', 'test1.mov']
+    assertEqual(mock_pysftp.Connection.get.call_count, 0)
