77278360175b27a7e565847574e3bc5ffee7afe3
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2019-07-19 13:18:38 +0100

    Updated test.py

--- app/test.py
+++ app/test.py
@@ -3,24 +3,22 @@ import video
 import subprocess
 import thermo
 import weight
-import power
-import output
-import video_ftp
+import rfid
 import pir
 
 
 def main_menu():
-    x = input("w = weight \nt = temp \np = post \nv = video \nf = ftp \nr = ftp \n")
+    x = input("temp=t\nvideo=v\nweight=w\nrfid=r\npir=p\n")
 
     if x == "t":
         temperature = thermo.sensor()
-        temperature.write(time=10, debug=True)
+        temperature.write(iterations=10, debug=True)
         temperature.avrg("temp_in.csv", "avrtempin.csv", debug=True)
         temperature.avrg("temp_out.csv", "avrtempout.csv", debug=True)
         main_menu()
 
     elif x == "v":
-        test = subprocess.Popen(["python3", "/home/pi/v0.8/video.py"])
+        test = subprocess.Popen(["python3", "/home/pi/HogPi/app/video.py"])
         test.wait()
         test.terminate()
         main_menu()
@@ -34,40 +32,14 @@ def main_menu():
         # weight_sensor.tare_weight(100)  # 100 = min tolerance
         main_menu()
 
-    elif x == "p":
-        web = post.http()
-        out = output.Output()
-        json_file = out.write_json(
-            "The Hedgehog Box of Doom",
-            "Jack Whitehorn",
-            "65 Horns Road",
-            "Stroud, Gloucstershire",
-            "Gl5 1EB",
-            "OK",
-            "gaberielbkyne@gmail.com",
-            "Jack Whitehorn",
-            "01453766796",
-            1,
-            "2019-01-19T00:00:00.000Z",
-            "0",
-            "0",
-            51.7429235,
-            -2.2057314,
-        )
-        web.post("https://hedgehog.bitnamiapp.com/api/boxes", json_file, True)
+    elif x == "r":
+        rfid_sensor = rfid.sensor()
+        rfid_sensor.read()
         main_menu()
 
-    elif x == "f":
-        new_ftp = video_ftp.ftp()
-        filename = new_ftp.send_video(
-            "hog_video",
-            "ftpk@robotacademy.co.uk",
-            "Angelgabe23",
-            "/",
-            box_id=1001,
-            hog_id=1234,
-        )
-        print(filename)
+    elif x == "p":
+        pir_sensor = pir.sensor(11)
+        pir_sensor.read()
         main_menu()
 
 
