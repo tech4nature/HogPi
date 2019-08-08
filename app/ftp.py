dde659cdfa7ec75a5d4f795349c288028a6c6f80
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2019-08-08 11:36:17 +0100

    Reverted changes because i forgot to switch branch

--- app/ftp.py
+++
@@ -1,7 +0,0 @@
-from ftplib import FTP
-
-
-def ftp_post(filename, file, username, password, ip):
-    ftp = FTP(ip)
-    ftp.login(user=username, passwd=password)
-    ftp.storbinary('STOR ' + filename, open(file, 'rb'))
