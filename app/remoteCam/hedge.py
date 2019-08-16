b5897a1754915e774dccd60087e1827384513ebb
Gabrielkyne <gabriel@kyne.co.uk>
2019-08-16 15:03:02 +0100

    Read the pir and only return 1 if 1 is returned for all the iterations specified in the function call.  500 is a good place to start with.

--- app/remoteCam/hedge.py
+++ app/remoteCam/hedge.py
@@ -10,10 +10,11 @@ irled.off()  # Turn led off
 
 
 while True:
-    #    hour = int(datetime.strftime(datetime.now(), '%H'))
-    #    if hour >= 22  or hour <= 6:
-    if pir_sensor.read() == 1:
-        print("PIR triggered")
-        irled.on()
-        subprocess.run(["python3", "video.py"])
-        irled.off()
+    hour = int(datetime.strftime(datetime.now(), '%H'))
+#   if hour > 0: # switch on for commissioning
+    if hour >= 22  or hour <= 6:
+        if pir_sensor.read(500) == 1:
+            print("PIR triggered")
+            irled.on()
+            subprocess.run(["python3", "video.py"])
+            irled.off()
