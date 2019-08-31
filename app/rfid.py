7d1a7f42bcf84deeed50c3e297326316f59f34a7
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2019-08-31 10:55:02 +0100

    Simplified rfid.py

--- app/rfid.py
+++ app/rfid.py
@@ -11,7 +11,7 @@ tag = None
 
 
 class sensor:
-    def __init__(self):
+    def __init__(self, rfid_record_time):
         global ser
         ser = serial.Serial(
             port="/dev/ttyAMA0",
@@ -19,18 +19,16 @@ class sensor:
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS,
-            timeout=10,
+            timeout=rfid_record_time,
         )
 
-    def read(self, rfid_record_time):
-        time_start = time.time() # record time
-        while (time.time() - time_start) < record_time # record for 120 secs unless read
-            ser.reset_input_buffer() #  clean buffer
-            ser.reset_output_buffer() # clean buffer
-            ser.write(b"sd2\r\n") # set mode of rfid
-            a = ser.read_until(size=19).decode(
-            "utf-8"
-            )  # 16 byte  + \r + \n somehow is 19 not 18
-            if len(a) > 15: # if read then return out of function
-                return a # return and break out of functions
+    def read(self):
+        ser.reset_input_buffer() #  clean buffer
+        ser.reset_output_buffer() # clean buffer
+        ser.write(b"sd2\r\n") # set mode of rfid
+        a = ser.read_until(size=19).decode(
+        "utf-8"
+        )  # 16 byte  + \r + \n somehow is 19 not 18
+        if len(a) > 15: # if read then return out of function
+            return a # return and break out of functions
         return "TagNotPresent" # return only if timed out
