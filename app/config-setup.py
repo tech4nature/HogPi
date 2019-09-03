174f3293a8f1e351ea9ced08728a6a988b80d9bc
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2019-09-03 10:59:28 +0100

    Change approach of setup script

--- app/config-setup.py
+++ app/config-setup.py
@@ -7,23 +7,20 @@ config = json.load(open('/home/pi/config.json', 'r'))
 def get_box_id():
     response = requests.get('http://trinity-stroud.hedgehogrepublic.org/api/locations/').json()
     location_list = response['results']
-    for i in location_list:
-        print(i['name'])
-    box_name = input('Box Name (Must be one of the above): ')
-    is_valid = False
-    for i in location_list:
-        if box_name == i['name']:
-            is_valid = True
-            config['box_id'] = i['code']
-            print('Box name was correct and the box id has been saved')
-    return is_valid
+    i = 0
+    for j in location_list:
+        i += 1
+        print(f'{i}) {j["name"]}')
+    box = int(input('Box Number: '))
+    config['box_id'] = location_list[box - 1]['code']
+    
 
 
 def main():
     print('Welcome to the HogPi Setup')
-    is_valid = get_box_id()
-    if is_valid == False:
-        print('The box name you entered is incorrect, please try again')
+    try:
+        get_box_id()
+    except Exception:
         get_box_id()
 
 if __name__ == '__main__':
