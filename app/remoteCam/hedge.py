7cf76e86080bd7afa2b531fe5e2746a693686ff3
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2019-12-05 10:21:40 +0000

    Added config.json, and added required dependencies to req file

--- app/remoteCam/hedge.py
+++ app/remoteCam/hedge.py
@@ -1,20 +1,32 @@
-
 import pir
 import subprocess
 from datetime import datetime
 import led
+import json
+from json_minify import json_minify
 
-pir_sensor = pir.sensor(4)
-irled = led.sensor(3)  # Instantiate led class and assign the pin the BCM3
+config = json.loads(
+    json_minify(open("config.json").read())
+)  # Removes comments from JSON then loads into python dictionaryjw
+
+pir_sensor = pir.sensor(config["pirPin"])
+irled = led.sensor(
+    config["ledPin"]
+)  # Instantiate led class and assign the pin the BCM3
 irled.off()  # Turn led off
 
 
 while True:
-    hour = int(datetime.strftime(datetime.now(), '%H'))
-#   if hour > 0: # switch on for commissioning
-    if hour >= 22  or hour <= 6:
-        if pir_sensor.read(500) == 1:
-            print("PIR triggered")
-            irled.on()
-            subprocess.run(["/usr/bin/python3", "/home/pi/HogPi/app/remoteCam/video.py"])
-            irled.off()
+    hour = int(datetime.strftime(datetime.now(), "%H"))
+    #   if hour > 0: # switch on for commissioning
+    if config["remoteCam"] == True:
+        if hour >= config["minTime"] or hour <= config["maxTime"]:
+            if pir_sensor.read(config["requiredPirIterations"]) == 1:
+                print("PIR triggered")
+                irled.on()
+                subprocess.run(
+                    ["/usr/bin/python3", "/home/pi/HogPi/app/remoteCam/video.py"]
+                )
+                irled.off()
+    if config["rfidTunnel"] == True:
+        pass  # RFID Tunnel code to go here
